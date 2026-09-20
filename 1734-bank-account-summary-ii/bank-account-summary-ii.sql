bd8034048c74fdf9ca9b14f1e928472bedb8de74
Harshini Bayella <harshinib49@gmail.com>
2026-09-20 15:52:55 +0530

    Time: 828 ms (82.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT u.name,SUM(t.amount) as balance
+FROM Users as u
+JOIN 
+Transactions as t
+ON u.account = t.account
+GROUP BY u.account,u.name
+HAVING SUM(t.amount) > 10000;
